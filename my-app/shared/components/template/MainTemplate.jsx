4a3ff7bed1d27d4f40812ccb937cfe13f3127dcf
chaemina <chaemina82@naver.com>
2024-06-30 16:12:27 +0900

    fix

--- my-app/shared/components/template/MainTemplate.jsx
+++ my-app/shared/components/template/MainTemplate.jsx
@@ -1,18 +1,17 @@
 import Title from "../atom/Title";
 import Intro from "../organism/Intro";
 import Experience from "../organism/Experience";
-import Project from "../organism/Project"
-
+import Project from "../organism/Project";
 
 export default function MainTemplate() {
   return (
-    <div className="w-full flex flex-col gap-5 sm:w-[600px] md:w-[728px] lg:w-[1000px] xl:w-[1260px] 2xl:w-[1300px] ">
+    <div className="w-full flex flex-col gap-5 sm:w-[600px] md:w-[728px] lg:w-[1000px] xl:w-[1100px] 2xl:w-[1300px] ">
       <Title>채민아 FrontEnd Developer</Title>
       <hr className="border-2 border-whiteorange" />
       <Intro />
-      <Experience/>
+      <Experience />
       <hr className="border-2 border-mint" />
-      <Project/>
+      <Project />
     </div>
   );
 }
